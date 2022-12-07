17eede32a200af7d6db2a89aaf12d54fb3cfbfaf
Josselin Bonnaud <josselin.bonnaud@gmail.com>
2022-12-07 10:20:46 +0100

    day 6

--- dec06/dec06.go
+++ dec06/dec06.go
@@ -9,7 +9,7 @@ func uniqueCharacters(s string) bool {
 
 	for i := 0; i < len(s)-1; i++ {
 		for j := i + 1; j < len(s); j++ {
-			if s[i:i+1] == s[j:j+1] {
+			if s[i] == s[j] {
 				return false
 			}
 		}
