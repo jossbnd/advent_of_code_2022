0cc3e5b1f1d18f64f1b23e32f5ceb84846bc1af9
Josselin Bonnaud <josselin.bonnaud@gmail.com>
2022-12-12 14:38:28 +0100

    day11 - more elegant solution than mine from peer

--- dec11/dec11.go
+++ dec11/dec11.go
@@ -1,133 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
-	"regexp"
 	"sort"
-	"strconv"
 	"strings"
 )
 
-func throw(monkey string, worryLevel int, receiver string, monkeys map[string]map[string][]string) {
-	item := strconv.Itoa(worryLevel)
-
-	monkeys[monkey]["items"] = monkeys[monkey]["items"][1:]
-	monkeys[receiver]["items"] = append(monkeys[receiver]["items"], item)
-
+type Monkey struct {
+	Items []int
+	Op    func(int) int
+	Test  func(int) int
 }
 
 func main() {
-	// Format input
-	dat, _ := os.ReadFile("./input11.txt")
-	input := strings.Split(string(dat), "\r\n")
+	input, _ := os.ReadFile("./input11.txt")
+	split := strings.Split(strings.TrimSpace(string(input)), "\r\n\r\n")
 
-	monkeys := make(map[int][]uint64)
-	monkey := 0
+	monkeys, lcm := make([]Monkey, len(split)), 1
+	for _, s := range split {
+		var items, op string
+		var i, v, test, t, f int
+		fmt.Sscanf(strings.NewReplacer(", ", ",", "* old", "^ 2").Replace(s),
+			`Monkey %d:
+  Starting items: %s
+  Operation: new = old %s %d
+  Test: divisible by %d
+    If true: throw to monkey %d
+    If false: throw to monkey %d`,
+			&i, &items, &op, &v, &test, &t, &f)
 
-	var operationSign string
-	var operationNumber uint64
-	var old bool
-	var test uint64
-	var receiverTrue int
-	var receiverFalse int
+		json.Unmarshal([]byte("["+items+"]"), &monkeys[i].Items)
 
-	var count [8]int
-
-	// Part Two
-	// lcm := uint64(19 * 3 * 13 * 7 * 5 * 11 * 17 * 2)
-
-	for i := 0; i < 20; i++ {
-		monkey = 0
-
-		for index, line := range input {
-
-			if i == 0 {
-				if index == 1+monkey*7 {
-					re := regexp.MustCompile(`[0-9]+`)
-					items := re.FindAllString(line, -1)
-					for _, val := range items {
-						item, _ := strconv.ParseUint(val, 10, 64)
-						monkeys[monkey] = append(monkeys[monkey], item)
-					}
-
-					continue
-				}
+		monkeys[i].Op = map[string]func(int) int{
+			"+": func(o int) int { return o + v },
+			"*": func(o int) int { return o * v },
+			"^": func(o int) int { return o * o },
+		}[op]
+		monkeys[i].Test = func(w int) int {
+			if w%test == 0 {
+				return t
 			}
-
-			if index == 2+monkey*7 {
-				l := strings.Split(string(line), " ")
-				operationSign = l[6]
-
-				if l[7] == "old" {
-					old = true
-				} else {
-					old = false
-				}
-
-				if !old {
-					operationNumber, _ = strconv.ParseUint(l[7], 10, 64)
-				}
-				continue
-			}
-
-			if index == 3+monkey*7 {
-				l := strings.Split(string(line), " ")
-				test, _ = strconv.ParseUint(l[5], 10, 64)
-				continue
-			}
-
-			if index == 4+monkey*7 {
-				l := strings.Split(string(line), " ")
-				receiverTrue, _ = strconv.Atoi(l[9])
-
-				continue
-			}
-
-			if index == 5+monkey*7 {
-				l := strings.Split(string(line), " ")
-				receiverFalse, _ = strconv.Atoi(l[9])
-				continue
-			}
-
-			if len(line) == 0 {
-				for _, worryLevel := range monkeys[monkey] {
-					count[monkey]++
-
-					if operationSign == "+" {
-						worryLevel += operationNumber
-					} else if operationSign == "*" && !old {
-						worryLevel *= operationNumber
-					} else if operationSign == "*" && old {
-						worryLevel *= worryLevel
-					}
-
-					worryLevel = worryLevel / 3
-					// worryLevel = worryLevel % lcm
-
-					var receiver int
-					if worryLevel%test == 0 {
-						receiver = receiverTrue
-					} else {
-						receiver = receiverFalse
-					}
-
-					monkeys[monkey] = monkeys[monkey][1:]
-					monkeys[receiver] = append(monkeys[receiver], worryLevel)
-
-				}
-
-				monkey++
-				continue
-			}
-
+			return f
 		}
-
+		lcm *= test
 	}
 
-	countSlice := count[:]
-	sort.Ints(countSlice)
-	fmt.Println("Part One Answer:", countSlice[len(countSlice)-1]*countSlice[len(countSlice)-2])
-	// fmt.Println("Part Two Answer:", countSlice[len(countSlice)-1]*countSlice[len(countSlice)-2])
+	fmt.Println("Part One Answer:", inspect(monkeys, 20, func(w int) int { return w / 3 }))
+	fmt.Println("Part Two Answer:", inspect(monkeys, 10000, func(w int) int { return w % lcm }))
+}
 
+func inspect(monkeys []Monkey, rounds int, op func(int) int) int {
+	monkeys = append([]Monkey{}, monkeys...)
+	inspected := make([]int, len(monkeys))
+	for i := 0; i < rounds; i++ {
+		for i, m := range monkeys {
+			for _, w := range m.Items {
+				w = op(m.Op(w))
+				monkeys[m.Test(w)].Items = append(monkeys[m.Test(w)].Items, w)
+				inspected[i]++
+			}
+			monkeys[i].Items = nil
+		}
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(inspected)))
+	return inspected[0] * inspected[1]
 }
